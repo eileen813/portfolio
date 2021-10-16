2e0f187fb2edf6a3597f1ea0b226b1f105a8b331
Eileen Olivera <eileenolivera@hotmail.com>
2021-10-16 17:17:54 -0400

    added Projects.jsx images and texts.  Updated styling throughout

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact comp</h1>
-    </div>
+    <>
+      <div className="email">
+        <h1>
+          <span>E</span>ileen<span>O</span>
+          livera@Hotmail.Com
+        </h1>
+      </div>
+
+      <div>
+        <a
+          href="https://www.linkedin.com/in/eileen-olivera/"
+          target="_blank"
+          noreferrer="noreferrer"
+        >
+          <h1>
+            Catch m<span>e o</span>n LinkedIn Here!
+          </h1>
+        </a>
+      </div>
+    </>
   );
 }
--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -1,9 +1,124 @@
 import React from "react";
+import "./Projects.css";
 
 export default function Projects() {
   return (
-    <div>
-      <h1>projects comp</h1>
+    <div className="project-container">
+      <div>
+        <img className="images" src="./cityslicker.png" alt="CitySlicker"></img>
+      </div>
+
+      <div className="texts">
+        <p>
+          An app that used the Teleport Public API to obtain requests from the
+          user to render on the browser for a specifically searched city’s
+          population, average temperatures, rent, salary, and available startup
+          jobs.
+        </p>
+        <a
+          href="https://eileen813.github.io/City-Slicker/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Deployed Site
+        </a>
+
+        <a
+          href="https://github.com/eileen813/City-Slicker"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Explore Code
+        </a>
+      </div>
+
+      <div>
+        <img className="images" src="./chorebox.png" alt="ChoreBox"></img>
+      </div>
+
+      <div className="texts">
+        <p>
+          An interactive React app implemented with Axios to obtain Airtable
+          data to render, create, edit, or delete one or all chores.
+        </p>
+        <a
+          href="https://chore-box.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Deployed Site
+        </a>
+
+        <a
+          href="https://github.com/eileen813/chorebox"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Explore Code
+        </a>
+      </div>
+
+      <div>
+        <img
+          className="images"
+          src="./tacato truck.png"
+          alt="Tacato Truck"
+        ></img>
+      </div>
+
+      <div className="texts">
+        <p>
+          A full stack Mongo/Express/React/Node(MERN Stack) app where a user can
+          sign in and render, create, edit, or delete one or all tacos from
+          their profile. This app was created with three other classmates. I
+          created the home page mission with its random carousel menu. I also
+          designed the main menu page that displays all menu items using
+          Material UIs Cards component.
+        </p>
+        <a
+          href="https://tacato-truck.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Deployed Site
+        </a>
+
+        <a
+          href="https://github.com/eileen813/TacatoTruck"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Explore Code
+        </a>
+      </div>
+
+      <div>
+        <img className="images" src="./homepro.png" alt="HomePro"></img>
+      </div>
+
+      <div className="texts">
+        <p>
+          An interactive full-stack, full CRUD app, using a RESTful JSON API
+          with the so-called "React on Rails" stack. The app allows user to sign
+          in and keep track of all their home remodeling projects, by means of
+          adding, editing, or deleting a project.
+        </p>
+        <a
+          href="https://home-pro.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Deployed Site
+        </a>
+
+        <a
+          href="https://github.com/eileen813/homepro"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Explore Code
+        </a>
+      </div>
     </div>
   );
 }
