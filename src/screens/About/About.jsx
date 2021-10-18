dd75a9ac2504e7cc0ba9b424340a8097c0b94580
Eileen Olivera <eileenolivera@hotmail.com>
2021-10-17 22:21:18 -0400

    updated about me section with image grid.  Added projects.css borders

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -1,26 +1,111 @@
 import React from "react";
+import "./About.css";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
 
 export default function About() {
   return (
-    <div>
-      <article>As a software engineer with 10+ years as a real estate professional, and a
-thorough understanding of company branding, and client satisfaction, I bring
-a people-facing perspective to the software engineering industry.  After earning my Associates and
-Bachelor's degrees in Information Technology and Health Information Systems,
-I pursued a career within the real estate title industry, working first as a
-typist/researcher.  I was quickly promoted to several positions of increased
-responsibility and decided to obtain my Sales Associate license.  This
-experience taught me the importance of great customer service along with
-excellent written and verbal communication skills.  As a person who always
-wondered what is happening behind the scenes as I input data into a program,
-or search for a specific data type, I decided to expand my skill set and
-further my education to pursue software engineering.  I graduated from the
-full time Software Engineering Immersive program at General Assembly, where
-I developed skills in JavaScript, HTML5, CSS, React, and other technical
-skills that allowed me to build four projects over the course of the
-twelve-week program. This experience, combined with many years in the real
-estate industry, helped me learn about problem-solving under pressure, and
-within strict timelines.  </article>
-    </div>
+    <>
+      <div>
+        <article>
+          As a software engineer with 10+ years as a real estate professional,
+          and a thorough understanding of company branding, and client
+          satisfaction, I bring a people-facing perspective to the software
+          engineering industry. When I'm not coding, I love spending time with
+          my family, my pups, gardening, and photography.
+        </article>
+      </div>
+
+      <ImageList
+        sx={{ width: 500, height: 450 }}
+        variant="quilted"
+        cols={4}
+        rowHeight={121}
+      >
+        {itemData.map((item) => (
+          <ImageListItem
+            key={item.img}
+            cols={item.cols || 1}
+            rows={item.rows || 1}
+          >
+            <img
+              {...srcset(item.img, 121, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
   );
 }
+
+const itemData = [
+  {
+    img: "./orchid.jpeg",
+    title: "Orchid",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "./hibiscus.jpeg",
+    title: "Hibiscus",
+  },
+  {
+    img: "./moo.jpeg",
+    title: "Moo-Moo",
+  },
+  {
+    img: "./benson.jpeg",
+    title: "Benson",
+    cols: 2,
+  },
+  {
+    img: "./orangerose.jpeg",
+    title: "Orange Rose",
+    cols: 2,
+  },
+  {
+    img: "./plumeria.jpeg",
+    title: "Plumeria",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "curly.jpeg",
+    title: "Curly",
+  },
+  {
+    img: "https://cdn.wallpapersafari.com/73/20/efEQvk.jpg",
+    title: "Acura",
+  },
+  {
+    img: "./collage.jpeg",
+    title: "collage",
+    rows: 3,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "./florida.jpeg",
+    title: "Florida",
+    rows: 3,
+    cols: 3,
+  },
+];
--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -4,34 +4,34 @@ import "./Projects.css";
 export default function Projects() {
   return (
     <div className="project-container">
-      <div>
+      <div className="cityslicker">
         <img className="images" src="./cityslicker.png" alt="CitySlicker"></img>
-      </div>
 
-      <div className="texts">
-        <p>
-          An app that used the Teleport Public API to obtain requests from the
-          user to render on the browser for a specifically searched city’s
-          population, average temperatures, rent, salary, and available startup
-          jobs.
-        </p>
-        <a
-          className="projects-link"
-          href="https://eileen813.github.io/City-Slicker/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Deployed Site
-        </a>
+        <div className="texts">
+          <p>
+            An app that used the Teleport Public API to obtain requests from the
+            user to render on the browser for a specifically searched city’s
+            population, average temperatures, rent, salary, and available
+            startup jobs.
+          </p>
+          <a
+            className="projects-link"
+            href="https://eileen813.github.io/City-Slicker/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Deployed Site
+          </a>
 
-        <a
-          className="projects-link"
-          href="https://github.com/eileen813/City-Slicker"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Explore Code
-        </a>
+          <a
+            className="projects-link"
+            href="https://github.com/eileen813/City-Slicker"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Explore Code
+          </a>
+        </div>
       </div>
 
       <div>
@@ -62,40 +62,40 @@ export default function Projects() {
         </a>
       </div>
 
-      <div>
+      <div className="tacato">
         <img
           className="images"
           src="./tacato truck.png"
           alt="Tacato Truck"
         ></img>
-      </div>
 
-      <div className="texts">
-        <p>
-          A full stack Mongo/Express/React/Node(MERN Stack) app where a user can
-          sign in and render, create, edit, or delete one or all tacos from
-          their profile. This app was created with three other classmates. I
-          created the home page mission with its random carousel menu. I also
-          designed the main menu page that displays all menu items using
-          Material UIs Cards component.
-        </p>
-        <a
-          className="projects-link"
-          href="https://tacato-truck.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Deployed Site
-        </a>
+        <div className="texts">
+          <p>
+            A full stack Mongo/Express/React/Node(MERN Stack) app where a user
+            can sign in and render, create, edit, or delete one or all tacos
+            from their profile. This app was created with three other
+            classmates. I created the home page mission with its random carousel
+            menu. I also designed the main menu page that displays all menu
+            items using Material UIs Cards component.
+          </p>
+          <a
+            className="projects-link"
+            href="https://tacato-truck.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Deployed Site
+          </a>
 
-        <a
-          className="projects-link"
-          href="https://github.com/eileen813/TacatoTruck"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Explore Code
-        </a>
+          <a
+            className="projects-link"
+            href="https://github.com/eileen813/TacatoTruck"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Explore Code
+          </a>
+        </div>
       </div>
 
       <div>
